be8abc1161ee75db47ceac89ab7624013f523e33
sw882882 <seungwan.wee@gmail.com>
2022-10-24 14:29:45 +0700

    toggleterm settings

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -4,20 +4,15 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
-	open_mapping = [[<c-\>]],
 	hide_numbers = true,
 	shade_terminals = true,
 	shading_factor = 2,
 	start_in_insert = true,
 	insert_mappings = true,
 	persist_size = true,
-	direction = "float",
+	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-	},
 })
 
 function _G.set_terminal_keymaps()
